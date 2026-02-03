d34535caac2f2eefeb2552f71529a43d54ecfdee
Mickael <crashbash161@gmail.com>
2026-02-03 17:04:51 +0100

    modification des fichiers

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-#Projet Anna
-Voici le projet Anna_Test.
-Il permet de tester le fonctionnement de **git**.
-## Objectifs
-Cette exploration doit permettre de réviser- [x] Markdown- [x] HTML- [x] JavaScript- [ ] Emmet
-Mais également découvrir le fonctionnement de git.
-![](https://www.01gifs.com/smileys/reflexion/1.gif)
+// Original Code By Webdevtrick ( https://webdevtrick.com )
+function showTime(){
+ var date = new Date();
+ var h = date.getHours();
+ var m = date.getMinutes();
+ var s = date.getSeconds();
+
+ h = (h < 10) ? "0" + h : h;
+ m = (m < 10) ? "0" + m : m;
+ s = (s < 10) ? "0" + s : s;
+
+ var time = h + ":" + m + ":" + s + " " + session;
+ document.getElementById("DigitalCLOCK").innerText = time;
+ document.getElementById("DigitalCLOCK").textContent = time;
+
+ setTimeout(showTime, 1000);
+
+}
+showTime();
